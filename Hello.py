3e44fcd0a0fef18fb7116cd315aba29f339c8601
Eric HB <107995930+eric-hb-carwow@users.noreply.github.com>
2024-01-09 23:19:32 +0000

    Another biiiig update

--- Hello.py
+++ Hello.py
@@ -6,6 +6,9 @@ import matplotlib.patches as patches
 if 'selections' not in st.session_state:
     st.session_state.selections = []
 
+if 'checks' not in st.session_state:
+    st.session_state.checks = 0
+
 evidence_type = ['AB test',
                 'Pre/post analysis',
                 'Big analysis',
@@ -36,6 +39,7 @@ risk_low = 5
 risk_mid = 10
 risk_high = 15
 risk_max = 20
+
 # Colours
 colours = {
     'Optional': '#38c399ff',
@@ -43,6 +47,27 @@ colours = {
     'Essential': '#1edce8ff'
 }
 
+
+# descriptions 
+main_description = """
+# When to test
+First calculate your confidence, next calculate your risk, then see where you end up on the board. 
+
+If you are in Essential, then let's build an AB test! If you're in optional or encouraged, the decision framework is in this notion doc to discuss with stakeholders and analytics.
+"""
+conf_description = """
+# Confidence
+To calculate your confidence, sum up your evidence. Choose the type of evidence you have and how aligned it is with your hypothesis. Once you've input those, press "Add" and your confidence score will go up.
+"""
+
+risk_description = """
+# Risk
+To calculate risk, first find the opportunity size (monthly traffic x conversion rate to revenue x average revenue). These are typically easy to find via amplitude, ask your local analyst for help if needed.
+
+Next, find the monthly revenue for your country and vertical, if the change will be in DE leasing, for example, you would use overall DE GYC revenue for the last month. This can be found in this tableau dashbaord, ask your local analyst for help if needed.
+
+Finally, input the change size.
+"""
 
 def make_plot(x, y):
     
@@ -95,7 +120,7 @@ def make_plot(x, y):
 
     plt.scatter(x, y, color='red', marker='o', s=200)
 
-    st.pyplot(plt)
+    # st.pyplot(plt)
 
     # check whether the point is in each area:
     essential_path = essential.get_path()
@@ -103,11 +128,13 @@ def make_plot(x, y):
 
     # Check if the point lies within the polygon
     if essential_path.contains_point((x, y)):
-        return 'Essential'
+        outcome = 'Essential'
     elif optional_path.contains_point((x, y)):
-        return 'Optional'
+        outcome = 'Optional'
     else:
-        return 'Encouraged'
+        outcome = 'Encouraged'
+
+    return (plt, outcome)
 
 def get_confidence():
 
@@ -130,7 +157,12 @@ def get_confidence():
     
     for idx, (type_name, final_value, amount) in enumerate(st.session_state.selections, start=1):
         total_confidence += final_value
-        
+    
+    if total_confidence <= 0:
+        total_confidence = 0.01
+    if total_confidence >= conf_max:
+        total_confidence = conf_max - 0.01
+
     return total_confidence
 
 def get_risk():
@@ -138,128 +170,139 @@ def get_risk():
     monthly_revenue = st.number_input('Enter the country & vertical monthly revenue')
     st.caption('To find revenue, see [this](https://10ay.online.tableau.com/#/site/carwowtest/views/MarketingDailyDashboard2_0/MarketingBusinessHealthDashboard) tableau dashboard')
     
-    change_size = st.slider('How large is the change?', min_value=0, max_value=100)
+    change_size = st.slider('How large is the change?', min_value=0, max_value=100, step=5)
     st.caption('Suggestion:  XS 5 | S 20 | M 50 | L 90 | XL 100')
     
-    if monthly_revenue == 0:
-        return {'opportunity': 0, 
+    if monthly_revenue * opp_size * change_size == 0:
+        risk = 0
+        return {'risk': 0,
+                'opportunity': 0, 
                 'revenue': 0, 
                 'change_size': 0}
     else:
-        return {'opportunity': opp_size, 
+        risk = opp_size / monthly_revenue * change_size
+    if risk >= risk_max:
+        risk = risk_max - .01
+    if risk <= 0:
+        risk = 0 + .01
+    return {'risk': risk,
+                'opportunity': opp_size, 
                 'revenue': monthly_revenue, 
                 'change_size': change_size}
 
+def get_alignment(amount):
+    if amount < -50:
+        alignment = 'opposed to'
+    elif amount < -10:
+        alignment = 'slightly opposed to'
+    elif amount < 50:
+        alignment = 'neutral to'
+    elif amount < 80:
+        alignment = 'mostly aligns with'
+    else:
+        alignment = 'highly aligned with'
+    return alignment
+
+def show_checkboxes():
+    st.markdown('#### Tick relevant boxes below')
+    option_1 = st.checkbox('Would a positive or negative result change a decision?')
+    option_2 = st.checkbox('Will anyone want to know the impact?')
+    option_3 = st.checkbox('Will learnings provide future value or influence future work?')
+    option_4 = st.checkbox('Do we want to have accurate measurement?')
+    option_5 = st.checkbox('Do we want to reduce risk further?')
+    option_6 = st.checkbox('Is there low opportunity cost to setup a test?')
+    st.session_state.checks = option_1 + option_2 + option_3 + option_4 + option_5 + option_6
+
+def show_confidence(confidence):
+    if confidence < conf_low:
+            conf_cat = 'Low'
+    elif conf_low <= confidence < conf_mid:
+        conf_cat = 'Mid'
+    elif conf_mid <= confidence < conf_high:
+        conf_cat = 'High'
+    else:
+        conf_cat = 'V High'
+    st.write(f'## Confidence: {conf_cat}')
+    if not bool(st.session_state.selections):
+        st.write('None')
+    for index, (type_name, final_value, amount) in enumerate(st.session_state.selections):
+        col1, col2 = st.columns([0.1, 0.9])
+        if col1.button(f"x", key=f"delete_{index}"):
+            del st.session_state.selections[index]
+            st.experimental_rerun()
+        
+        alignment = get_alignment(amount)
+        col2.write(f"{type_name} {alignment} our hypothesis")
+
+def show_risk(risk_values):
+    if risk_values['risk'] < risk_low:
+        risk_cat = 'V Low'
+    elif risk_low <= risk_values['risk'] < risk_mid:
+        risk_cat = 'Low'
+    elif risk_mid <= risk_values['risk'] <= risk_high:
+        risk_cat = 'Mid'
+    else:
+        risk_cat = 'High'
+
+    st.markdown(f'## Risk: {risk_cat}')
+    st.caption(f'Opportunity: {risk_values["opportunity"]}  \n Monthly revenue: {risk_values["revenue"]}  \n Change size: {risk_values["change_size"]}')
+
+def show_outcome(outcome):
+    if outcome == 'Essential':
+        st.write('## AB test!')
+
+    elif outcome == 'Encouraged':
+        if st.session_state.checks > 0:
+            st.write('## AB test!')
+        else: 
+            st.write('## Measure but no AB test')
+    
+    else:
+        if st.session_state.checks > 3:
+            st.write('## AB test!')
+        elif st.session_state.checks > 1:
+            st.write('## Measure but no AB test')
+        else:
+            st.write('## Roll it out!')
+
 def main():
-    st.write("""
-    # When to test
-    First calculate your confidence, next calculate your risk, then see where you end up on the board. 
-
-    If you are in Essential, then let's build an AB test! If you're in optional or encouraged, the decision framework is in this notion doc to discuss with stakeholders and analytics.
-    """)
-
+    st.write(main_description)
+    
+    # User inputs for confidence and risk
     left_column, _, right_column = st.columns([1, 0.2, 1])
     with left_column:
-        st.write("""
-        # Confidence
-        To calculate your confidence, sum up your evidence. Choose the type of evidence you have and how aligned it is with your hypothesis. Once you've input those, press "Add" and your confidence score will go up.
-        """)
+        st.write(conf_description)
         confidence = get_confidence()
-        if confidence <= 0:
-            confidence = 0.01
-        if confidence >= conf_max:
-            confidence = conf_max - 0.01
+
     with right_column:
-        st.write("""
-        # Risk
-        To calculate risk, first find the opportunity size (monthly traffic x conversion rate to revenue x average revenue). These are typically easy to find via amplitude, ask your local analyst for help if needed.
-
-        Next, find the monthly revenue for your country and vertical, if the change will be in DE leasing, for example, you would use overall DE GYC revenue for the last month. This can be found in this tableau dashbaord, ask your local analyst for help if needed.
-
-        Finally, input the change size.
-        """)
-         
+        st.write(risk_description)
         risk_values = get_risk()
-        if risk_values['change_size'] == 0:
-            risk = 0
-        else:
-            risk = risk_values['opportunity'] / risk_values['revenue'] * risk_values['change_size']
-        if risk >= risk_max:
-            risk = risk_max - .01
-        if risk <= 0:
-            risk = 0 + .01
+
     st.write(' ')
-    
-    left_column2, _, right_column2 = st.columns([1, 0.2, 1])
+    # Showing the user their inputs for confidence and risk
+    left_column2, _, right_column2 = st.columns([1, 0.1, .8])
     with left_column2:
-        if confidence < conf_low:
-            conf_cat = 'Low'
-        elif conf_low <= confidence < conf_mid:
-            conf_cat = 'Mid'
-        elif conf_mid <= confidence < conf_high:
-            conf_cat = 'High'
-        else:
-            conf_cat = 'V High'
-        st.write(f'## Confidence: {conf_cat}')
-        if not bool(st.session_state.selections):
-            st.write('None')
-        for (type_name, final_value, amount) in st.session_state.selections:
-            if amount < -50:
-                alignment = 'opposed to'
-            elif amount < -10:
-                alignment = 'slightly opposed to'
-            elif amount < 50:
-                alignment = 'neutral to'
-            elif amount < 80:
-                alignment = 'mostly aligns with'
-            else:
-                alignment = 'highly aligned with'
-            st.write(f"{type_name} {alignment} our hypothesis")
-    
+        show_confidence(confidence)
+            
     with right_column2:
-        if risk < risk_low:
-            risk_cat = 'V Low'
-        elif risk_low <= risk < risk_mid:
-            risk_cat = 'Low'
-        elif risk_mid <= risk <= risk_high:
-            risk_cat = 'Mid'
-        else:
-            risk_cat = 'High'
+        show_risk(risk_values)
 
-        st.write(f'## Risk: {risk_cat}')
-        st.write(f'Opportunity: {risk_values["opportunity"]}')
-        st.write(f'Monthly revenue: {risk_values["revenue"]}')
-        st.write(f'Change size: {risk_values["change_size"]}')
+    # Showing the graph and outcome
+    (plt, outcome) = make_plot(risk_values['risk'], confidence)
+
     
-    st.write(' ')
-    _, graph, _ = st.columns([.2, 1, .5])
-    with graph:
-        outcome = make_plot(risk, confidence)
-    st.markdown(f'# AB testing is <span style="color: {colours[outcome]};">{outcome}</span>', unsafe_allow_html=True)
+    checkbox_col, _, graph = st.columns([.8, .1, 1])
 
-    if outcome != 'Essential':
-        st.write('Please check the boxes below that are relevant for your change')
-        option_1 = st.checkbox('Would a positive or negative result change a decision?')
-        option_2 = st.checkbox('Will anyone want to know the impact?')
-        option_3 = st.checkbox('Will learnings provide future value or influence future work?')
-        option_4 = st.checkbox('Do we want to have accurate measurement?')
-        option_5 = st.checkbox('Do we want to reduce risk further?')
-        option_6 = st.checkbox('Is there low opportunity cost to setup a test?')
-        a = option_1 + option_2 + option_3 + option_4 + option_5 + option_6
-        
-        if outcome == 'Encouraged':
-            if a > 0:
-                st.write('# AB test!')
-            else: 
-                st.write('# Measure impact without an AB test')
-        
-        if outcome == 'Optional':
-            if a > 3:
-                st.write('# AB test!')
-            elif a > 1:
-                st.write('# Measure impact without an AB test')
-            else:
-                st.write('# Let it roll without measurement')
+    with checkbox_col:
+        show_checkboxes()
+
+    with graph:
+        st.markdown(f'### AB testing is <span style="color: {colours[outcome]};">{outcome}</span>', unsafe_allow_html=True)
+        st.pyplot(plt)
+        show_outcome(outcome)
+    
+    
+
         
         
 
